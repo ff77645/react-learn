8a5119f4511798495046297246bd19585bf48fbf
fengy <49145107+ff77645@users.noreply.github.com>
2022-05-09 11:11:46 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const JsxH1 = <h1>title</h1>;
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>title</h1>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
